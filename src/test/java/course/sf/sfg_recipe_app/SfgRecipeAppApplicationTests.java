e225c4988530570879acc37e34d7db35b670e425
pratikdk <pratik.deoolwadikar@gmail.com>
2021-07-31 12:58:12 +0530

    first commit

---
+++ src/test/java/course/sf/sfg_recipe_app/SfgRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package course.sf.sfg_recipe_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
